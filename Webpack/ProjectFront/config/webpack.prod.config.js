30742c19b6e9c46e8fc143c0526767c06a55d26b
yuluo <2216847528@qq.com>
2024-05-21 14:50:35 +0800

    updates for webpack

--- Webpack/ProjectFront/config/webpack.prod.config.js
+++ Webpack/ProjectFront/config/webpack.prod.config.js
@@ -2,7 +2,7 @@ const {merge} = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
+const webpack = require ('webpack');
 module.exports = merge(baseConfig, {
     // loaders
     module: {
@@ -27,13 +27,13 @@ module.exports = merge(baseConfig, {
             SERVICE_URL: JSON.stringify('http://api.fuming.site:54254')
         })
     ],
-    optimization: {
-        minimizer: [
-            // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
-            `...`,
-            new CssMinimizerPlugin(),
-        ],
-    },
+    // optimization: {
+    //     minimizer: [
+    //         // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+    //         `...`,
+    //         new CssMinimizerPlugin(),
+    //     ],
+    // },
     // 模式
     mode: 'production',
     // sourceMap
