b7ef457430e3d99e1d50055905e29d2fa841d1ff
yuluo <2216847528@qq.com>
2024-07-17 17:39:57 +0800

    shop update for use vuex save data

---
+++ Vue/vue-cli/shopbyvue/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+// 导入product模块
+import product from "@/store/product";
+
+// 挂载
+Vue.use(Vuex);
+// 定义store对象
+const store = new Vuex.Store({
+    modules:{
+        product,
+    }
+});
+// 导出store对象
+export default store;
---
+++ Vue/vue-cli/shopbyvue/src/store/product/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// 定义商品的数据状态
+const state = {
+    // 首页分类列表
+    categoryList:[],
+}
+// 定义mutations
+const mutations = {
+
+}
+// 定义actions
+const actions = {
+
+}
+// 暴漏数据，导出模块
+export default {
+    namespace:true,
+    state,
+    mutations,
+    actions,
+}
