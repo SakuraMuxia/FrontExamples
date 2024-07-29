13b9d27cb807baf80f88bc4f8a5fe3db8489f50b
yuluo <2216847528@qq.com>
2024-07-29 17:33:27 +0800

    update for a letter

--- Vue/vue-cli/shopbyvue/src/utils/gotoSearch.js
+++ Vue/vue-cli/shopbyvue/src/utils/gotoSearch.js
@@ -3,7 +3,7 @@ import VueRouter from "vue-router";
 
 // 接收参数对象
 VueRouter.prototype.gotoSearch = function (query) {
-    console.log(this)
+    // console.log(this)
     this.push({
         path: "/search",
         query: {
