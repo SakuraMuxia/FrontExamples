41e4654ca3288e2bac3a43050f4ab4946b824112
yuluo <2216847528@qq.com>
2024-08-16 17:36:48 +0800

    update for vue3

--- Vue3/Test/demo/vue.config.js
+++ Vue3/Test/demo/vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    lintOnSave:false,
+    devServer: {
+        open: true,// 自动打开浏览器
+        port: 80,// 指定端口号
+        host: "zhangpeiyue.com",// 指定host
+    }
 })
