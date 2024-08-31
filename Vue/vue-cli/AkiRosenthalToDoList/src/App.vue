4991bcd607dcf5cafce62053a1f11c7328a268c3
yuluo <2216847528@qq.com>
2024-08-31 09:49:22 +0800

    update for vue3

--- Vue/vue-cli/AkiRosenthalToDoList/src/App.vue
+++ Vue/vue-cli/AkiRosenthalToDoList/src/App.vue
@@ -32,3 +32,4 @@ export default {
         border-radius: 5px;
     }
 </style>
+
--- Vue3/Test/demo/vue.config.js
+++ Vue3/Test/demo/vue.config.js
@@ -4,7 +4,17 @@ module.exports = defineConfig({
     lintOnSave:false,
     devServer: {
         open: true,// 自动打开浏览器
-        port: 80,// 指定端口号
-        host: "zhangpeiyue.com",// 指定host
+        port: 8080,// 指定端口号
+        host: "127.0.0.1",// 指定host
+    },
+    chainWebpack: (config) => {
+        config.plugin('define').tap((definitions) => {
+            Object.assign(definitions[0], {
+                __VUE_OPTIONS_API__: 'true',
+                __VUE_PROD_DEVTOOLS__: 'false',
+                __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
+            })
+            return definitions
+        })
     }
 })
